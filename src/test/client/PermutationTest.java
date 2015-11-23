a4b44bb6a3167162e427786f8b4e0a433c3d9ef9
SAMBATH <samkumar15@gmail.com>
2015-11-23 18:18:31 +0100

    Initial commit

--- src/test/client/PermutationTest.java
+++ src/test/client/PermutationTest.java
@@ -6,10 +6,8 @@ import com.google.gwt.core.client.EntryPoint;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class PermutationTest implements EntryPoint {
-	
+
 	public void onModuleLoad() {
 
-//		AC ac = GWT.create(AC.class);
-//		ac._do();
 	}
 }
